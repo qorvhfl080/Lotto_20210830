a41c48e4dfd9c5e1eeb460e44184f1042fd873b1
qorvhfl080 <86284854+qorvhfl080@users.noreply.github.com>
2021-08-30 11:58:04 +0900

    잘못된 숫자를 넣었다면 다시 입력받기

--- src/code/Lotto.java
+++ src/code/Lotto.java
@@ -5,20 +5,31 @@ import java.util.Scanner;
 public class Lotto {
 
 	public static void main(String[] args) {
-		
+
 //		입력한 6개의 숫자를 저장할 배열
 		int[] inputNumbers = new int[6];
-		
+
 //		6개의 숫자에 Scanner를 이용해서 숫자 입력
 		Scanner sc = new Scanner(System.in);
-		
-		for (int i=0; i < inputNumbers.length; i++) {
-			System.out.print((i+1) + "번째 숫자 입력 : ");
-			inputNumbers[i] = sc.nextInt();
+
+		for (int i = 0; i < inputNumbers.length; i++) {
+
+			while (true) {
+				System.out.print((i + 1) + "번째 숫자 입력 : ");
+				int num = sc.nextInt();
+				
+				if (false) {
+					break;
+				} else {
+					System.out.println("잘못된 숫자입니다. 다시 입력해주세요");
+				}
+				
+			}
+
 		}
-		
+
 		sc.close();
-		
+
 	}
 
 }
