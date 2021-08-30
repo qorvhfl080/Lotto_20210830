cfe2275ec5925e947f584b9a13a4d47ae1fd5a19
qorvhfl080 <86284854+qorvhfl080@users.noreply.github.com>
2021-08-30 11:53:40 +0900

    Scanner 이용하여 숫자 입력받기

--- src/code/Lotto.java
+++ src/code/Lotto.java
@@ -1,10 +1,23 @@
 package code;
 
+import java.util.Scanner;
+
 public class Lotto {
 
 	public static void main(String[] args) {
 		
-		// 가나다
+//		입력한 6개의 숫자를 저장할 배열
+		int[] inputNumbers = new int[6];
+		
+//		6개의 숫자에 Scanner를 이용해서 숫자 입력
+		Scanner sc = new Scanner(System.in);
+		
+		for (int i=0; i < inputNumbers.length; i++) {
+			System.out.print((i+1) + "번째 숫자 입력 : ");
+			inputNumbers[i] = sc.nextInt();
+		}
+		
+		sc.close();
 		
 	}
 
